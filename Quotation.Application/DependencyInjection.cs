57fab299e08624db0a4bc1cd6f6a46eeae2b058f
Hihiz <public@mail.ru>
2023-12-16 01:47:06 +0300

    Имплементация интерфейса, регистрация зависимостей, подключение NuGet пакетов

---
+++ Quotation.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quotation.Application.Interfaces;
+using Quotation.Application.Services;
+
+namespace Quotation.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.ServicesInit();
+
+            return services;
+        }
+
+        private static void ServicesInit(this IServiceCollection services)
+        {
+            services.AddScoped<IQuotationService, QuotationService>();
+        }
+    }
+}
---
+++ Quotation.Application/Services/QuotationService.cs
@@ -0,0 +1,57 @@
+﻿using Quotation.Application.Interfaces;
+using Quotation.Application.Models;
+using Quotation.Domain.Entities;
+using Quotation.Domain.Enums;
+using System.Xml;
+
+namespace Quotation.Application.Services
+{
+    public class QuotationService : IQuotationService
+    {
+        public async Task<IBaseStatus<List<Valute>>> GetQuotationsAsync(DateOnly date)
+        {
+            List<Valute> valuteList = new List<Valute>();
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync($"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}"))
+                {
+                    XmlDocument xmlDocument = new XmlDocument();
+
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    xmlDocument.Load($"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}");
+
+                    foreach (XmlNode node in xmlDocument.SelectNodes("ValCurs/Valute"))
+                    {
+                        valuteList.Add(new Valute()
+                        {
+                            Id = node.Attributes["ID"].Value,
+                            NumCode = node["NumCode"]?.InnerText,
+                            CharCode = node["CharCode"]?.InnerText,
+                            Nominal = node["Nominal"]?.InnerText,
+                            Name = node["Name"]?.InnerText,
+                            Value = node["Value"]?.InnerText,
+                            VunitRate = node["VunitRate"]?.InnerText
+                        });
+                    }
+
+                    if (valuteList.Count == 0)
+                    {
+                        return new StatusResponseError<List<Valute>>()
+                        {
+                            StatusCode = (int)ErrorCode.NotFound,
+                            Message = ErrorCode.NotFound.ToString(),
+                        };
+                    }
+                }
+            }
+
+            return new StatusResponse<List<Valute>>()
+            {
+                StatusCode = (int)ErrorCode.OK,
+                Data = valuteList
+            };
+        }
+    }
+}
