be51a1db1b31f740ce27dd329c8b013d1c4af7f2
Hihiz <public@mail.ru>
2023-12-16 01:49:36 +0300

    Регистрация зависимостей, подключение NuGet пакетов

---
+++ Quotation.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Quotation.Application.Interfaces;
+using Quotation.Infrastructure.Data;
+using Quotation.Infrastructure.Identity;
+using Quotation.Infrastructure.Interfaces;
+using Quotation.Infrastructure.Services;
+using System.Text;
+
+namespace Quotation.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionLocalDb");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            services.ServicesInit();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"]!,
+                        ValidAudience = configuration["Jwt:Audience"]!,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]!))
+                    };
+                });
+
+            services.AddAuthorization(options => options.DefaultPolicy =
+                new AuthorizationPolicyBuilder
+                    (JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build());
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 1;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole<long>>()
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //services.AddIdentity<ApplicationUser, IdentityRole<long>>()
+            //.AddEntityFrameworkStores<ApplicationDbContext>()
+            //.AddUserManager<UserManager<ApplicationUser>>()
+            //.AddSignInManager<SignInManager<ApplicationUser>>();
+
+            return services;
+        }
+
+        private static void ServicesInit(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
+        }
+    }
+}
